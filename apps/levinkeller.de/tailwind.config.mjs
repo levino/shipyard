654eef926d13816804de25af58fe701f632eccb5
Levin Keller <post@levinkeller.de>
2024-01-22 09:38:49 +0100

    fix: improve navigation bar behaviour on mobile

--- apps/levinkeller.de/tailwind.config.mjs
+++ apps/levinkeller.de/tailwind.config.mjs
@@ -2,7 +2,7 @@
 export default {
   content: [
     './src/**/*.{ts,tsx,md,mdx,astro}',
-    'node_modules/@shipyard/**/*.{tsx,astro}',
+    '../../shipyard/**/*.{tsx,astro}',
   ],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
