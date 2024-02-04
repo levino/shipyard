1a29d87b664629994b3c37230eca9422574fe7c0
Levin Keller <post@levinkeller.de>
2024-02-04 17:27:19 +0100

    feat: add more veg

---
+++ shipyard/garden-planner/components/VegetableTable.tsx
@@ -0,0 +1,32 @@
+import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons'
+import type { CollectionEntry } from 'astro:content'
+import type { FC } from 'react'
+
+export const VegetableTable: FC<{
+  vegetables: CollectionEntry<'vegetables'>[]
+  caption: string
+}> = ({ vegetables, caption }) => (
+  <table className="table">
+    <p>{caption}</p>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>deutscher Name</th>
+        <th>vorrätig</th>
+      </tr>
+    </thead>
+    <tbody>
+      {vegetables.map((vegetable) => (
+        <tr key={vegetable.id}>
+          <th>
+            <a href={`/de/garden/plants/${vegetable.id}`}>
+              {vegetable.data.name.latin}
+            </a>
+          </th>
+          <td>{vegetable.data.name.common}</td>
+          <td>{vegetable.data.inStock ? <CheckIcon /> : <Cross2Icon />}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
