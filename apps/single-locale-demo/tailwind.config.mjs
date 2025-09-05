bedbc7c9f9bceecf8e5523f80c382b509f177406
Levin Keller <post@levinkeller.de>
2025-09-05 13:57:44 +0000

    --wip-- [skip ci]

--- apps/single-locale-demo/tailwind.config.mjs
+++ apps/single-locale-demo/tailwind.config.mjs
@@ -1,6 +1,22 @@
+import path from 'node:path'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './{blog,docs,src}/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    path.join(
+      path.dirname(require.resolve('@levino/shipyard-base')),
+      '../astro/**/*.astro',
+    ),
+    path.join(
+      path.dirname(require.resolve('@levino/shipyard-docs')),
+      '../astro/**/*.astro',
+    ),
+    path.join(
+      path.dirname(require.resolve('@levino/shipyard-blog')),
+      '../astro/**/*.astro',
+    ),
+  ],
   theme: {
     extend: {},
   },
